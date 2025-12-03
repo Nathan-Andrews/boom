6732e3d4fe197b52ec573bd70a7c7cc8418c5878
Nathan-Andrews <andrewsnathan2003@gmail.com>
2025-12-03 16:00:27 -0600

    build dropdown in popup

--- .boomserver/static/scripts/popup.js
+++ .boomserver/static/scripts/popup.js
@@ -3,11 +3,28 @@ createChannelConnectionOnPopup();
 buildDropdown();
 
 async function buildDropdown() {
-    fetch('/static/styles/themes/theme-metadata.json')
-    .then(response => response.text())
-    .then(data => {
-      console.log(data)
-    })
-    .catch(error => console.error('Error loading text file:', error));
+    try {
+        const response = await fetch('/static/styles/themes/theme-metadata.json');
+        const json = await response.json();
 
+        const dropdown = document.getElementById("theme-dropdown");
+        dropdown.innerHTML = ""; 
+
+        for (const theme of json.themes) {
+            const option = document.createElement("option");
+            option.value = theme.id;
+            option.textContent = `${theme.name}`;
+            // option.textContent = `${theme.name} — ${theme.author}`;
+            dropdown.appendChild(option);
+        }
+
+    } catch (error) {
+        console.error('Error loading metadata:', error);
+    }
+}
+
+function handleDropdownChange(themeDropdown) {
+    console.log(themeDropdown.value)
+    setTheme(themeDropdown.value)
+    publishToChannel(themeDropdown.value)
 }
